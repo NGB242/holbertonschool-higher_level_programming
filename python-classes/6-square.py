da263c0e55d3e827ebc23ec49661bec0eed07fff
NGB242 <ngbwarren@gmail.com>
2025-01-29 04:19:42 +0100

    display the result of a class Square that defines a square by: (based on 5-square.py)

---
+++ python-classes/6-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        self.__size = value
+        
