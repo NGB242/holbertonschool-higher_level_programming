dc217f69fd3e6a28470c27ed164a8a2da6176222
NGB242 <ngbwarren@gmail.com>
2025-01-31 08:30:28 +0100

    display the result of correction

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is the Square module who initialize a square class,
+with a private class attribute "size"
+"""
+
+
+class Square():
+    """initialize a square's class with a private class atribute"""
+    def __init__(self, size=0):
+        """"initialize the size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """"getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area of the square"""
+        return self.__size * self.__size
+ 
