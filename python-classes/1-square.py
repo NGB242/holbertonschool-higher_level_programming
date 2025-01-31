bd04d49aefa2557a01746821ad9805e77b540e41
NGB242 <ngbwarren@gmail.com>
2025-01-31 08:35:05 +0100

    display the result of correction

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        """Initialise un nouveau carré.
-        
-        Args:
-            size (int): La taille d'un côté du carré. Par défaut, elle est 0.
-        """
-        self.__size = size
+"""
+This is the Square module who initialize a square class,
+with a private class attribute "size"
+"""
 
-    def area(self):
-        """Calcule l'aire du carré.
-        
-        Returns:
-            int: L'aire du carré.
-        """
-        return self.__size ** 2
+
+class Square():
+    """initialize a square's class with a private class atribute"""
+    def __init__(self, size):
+        self.__size = size
