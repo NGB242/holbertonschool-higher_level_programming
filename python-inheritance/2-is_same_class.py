e61c57fadc4fadf717e22e1c043611083d50b3f9
NGB242 <ngbwarren@gmail.com>
2025-02-05 16:43:39 +0100

    display the coorection of error

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 
-def is_exact_instance(obj, cls):
-    return type(obj) is cls
-
-
-class MyClass:
-    pass
-
-class AnotherClass:
-    pass
-
-obj1 = MyClass()
-obj2 = AnotherClass()
-
-
-print(is_exact_instance(obj1, MyClass))  # Output: True
-print(is_exact_instance(obj2, MyClass))  # Output: False
-print(is_exact_instance(obj1, AnotherClass))  # Output: False
+def is_same_class(obj, a_class):
+    """
+    Check if an object is exactly an instance of a specified class.
+    """
+    return type(obj) is a_class
