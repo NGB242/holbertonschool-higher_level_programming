3ff53d2f0cc2e5a88ddc26f2088d2f3cab340cdf
NGB242 <ngbwarren@gmail.com>
2025-02-05 12:27:44 +0100

    display the correction

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,14 +4,26 @@ def lookup(obj):
     return dir(obj)
 
 
-class Parent:
-    def parent_method(self):
+class Parent: 
+
+    """ Definition of the Parent class """
+    
+    def parent_method(self): 
+        """ Parent class method """
     pass
+    """ This method does nothing for the moment"""
 
 class Child(Parent):
-    def child_method(self):
+
+    """ Definition of the Child class, which inherits from Parent """
+    
+    def child_method(self): 
+        """ Method specific to the Child class """
     pass
+    """ This method does nothing for the moment """
 
 
 child_instance = Child()
+""" Creating an instance of the Child class """ 
 print(dir(child_instance))
+""" Displays all methods and attributes accessible for child_instance """
