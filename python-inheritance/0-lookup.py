bc04e2c6a71132a2cb331b6400a0a3e92067011d
NGB242 <ngbwarren@gmail.com>
2025-02-05 12:34:58 +0100

    the reel correction of taks

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,28 +4,3 @@ def lookup(obj):
     Returns the list of attributes and methods available for an object.
     """
     return dir(obj)
-
-
-class Parent: 
-
-    """ Definition of the Parent class """
-    
-    def parent_method(self): 
-        """ Parent class method """
-    pass
-    """ This method does nothing for the moment"""
-
-class Child(Parent):
-
-    """ Definition of the Child class, which inherits from Parent """
-    
-    def child_method(self): 
-        """ Method specific to the Child class """
-    pass
-    """ This method does nothing for the moment """
-
-
-child_instance = Child()
-""" Creating an instance of the Child class """ 
-print(dir(child_instance))
-""" Displays all methods and attributes accessible for child_instance """
