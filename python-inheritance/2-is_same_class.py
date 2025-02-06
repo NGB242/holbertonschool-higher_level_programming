d00d9852f29f9247a253dc9d545683680e9275b6
NGB242 <ngbwarren@gmail.com>
2025-02-06 08:14:51 +0100

    display the result of reel correction

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+This script contains a function that checks if an object is exactly an instance
+of a specified class.
+"""
+
 
 def is_same_class(obj, a_class):
-    """
-    Check if an object is exactly an instance of a specified class.
-    """
-    return type(obj) is a_class
+        """
+        Check if an object is exactly an instance of a specified class.
+        """
+        return type(obj) is a_class
