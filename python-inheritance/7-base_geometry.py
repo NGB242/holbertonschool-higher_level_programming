582c656c51accdaf961eb63b59e6120150a50e95
NGB242 <ngbwarren@gmail.com>
2025-02-06 14:22:42 +0100

    display the correction of a task7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,17 +2,19 @@
 
 class BaseGeometry:
     def area(self):
-        """Raises an exception indicating that the area method is not implemented."""
+        """
+        Raises an exception indicating that the area method is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         Validates that 'value' is an integer and greater than 0.
-        
+
         Parameters:
         name (str): The name of the parameter.
         value (int): The value to validate.
-        
+
         Raises:
         TypeError: If 'value' is not an integer.
         ValueError: If 'value' is less than or equal to 0.
