5dc8d7eac8e615a641c47d3895565cd5bd3a369d
NGB242 <ngbwarren@gmail.com>
2025-02-06 11:23:11 +0100

    display the result of a reel correction of a task5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry.
+"""
+
+
 class BaseGeometry:
+    """
+    A base class for geometric operations.
+    Currently, it does not contain any attributes or methods.
+    """
     pass
