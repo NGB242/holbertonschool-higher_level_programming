87f8ca7dc929aaefd1bc1cb4237594127350d3a5
NGB242 <ngbwarren@gmail.com>
2025-02-06 11:28:14 +0100

    correction of task6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+Defines a class BaseGeometry with an unimplemented area method.
+"""
+
+
 class BaseGeometry:
+    """
+    A base class for geometric operations.
+    """
+
     def area(self):
+        """
+        Raises an exception to indicate that the area method is not implemented.
+
+        This method is intended to be overridden in subclasses that provide 
+        a specific implementation for calculating the area.
+        
+        Raises:
+            Exception: Always raises an exception indicating the method is not implemented.
+        """
         raise Exception("area() is not implemented")
