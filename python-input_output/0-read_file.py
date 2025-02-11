94ab49dbcebee06e95c17b64686ab28ba820ab50
NGB242 <ngbwarren@gmail.com>
2025-02-11 10:35:35 +0100

    reel correction of task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
+"""
+Ouvre un fichier en lecture et affiche son contenu sur la console.
+
+:param filename: Nom du fichier à lire.
+"""
+
+
 def read_file(filename=""):
     """
     Ouvre un fichier en lecture et affiche son contenu sur la console.
 
     :param filename: Nom du fichier à lire.
     """
-
-
-def read_file(filename):
-    """
-    Ouvre un fichier en lecture et affiche son contenu sur la console.
-
-    :param filename: Nom du fichier à lire.
-    """
-
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end='')
