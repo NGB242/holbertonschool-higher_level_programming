b5c3948811bbcab0feb881f42d2d5fd974ba24e2
NGB242 <ngbwarren@gmail.com>
2025-02-13 16:56:22 +0100

    0-read_file.py correction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""
-Ouvre un fichier en lecture et affiche son contenu sur la console.
-
-:param filename: Nom du fichier à lire.
-"""
+"""function that print in the output the content of a folder"""
 
 
 def read_file(filename=""):
-    """
-    Ouvre un fichier en lecture et affiche son contenu sur la console.
-
-    param filename: Nom du fichier à lire.
-    """
+    """function that print in the output the content of a folder"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end='')
