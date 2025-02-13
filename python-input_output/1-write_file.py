e392b70bed7b69860fcde1983671f455891605c6
NGB242 <ngbwarren@gmail.com>
2025-02-13 17:05:56 +0100

    display the reel correction

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Writes a string to a text file in UTF-8 encoding and returns the number of characters written.
+Writes a string to a text file in UTF-8 encoding and,
+returns the number of characters written.
 
 param filename: The name of the file to write to.
 param text: The string to write to the file.
@@ -10,7 +11,8 @@ return: The number of characters written.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file in UTF-8 encoding and returns the number of characters written.
+    Writes a string to a text file in UTF-8 encoding and,
+    returns the number of characters written.
 
     :param filename: The name of the file to write to.
     :param text: The string to write to the file.
