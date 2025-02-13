926a6e8d07161a780ca55978f42eb76d99c33f23
NGB242 <ngbwarren@gmail.com>
2025-02-13 17:43:14 +0100

    6-load_from_json_file.py correction

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,17 +10,10 @@ import json
 def load_from_json_file(filename):
     """
     Load data from a JSON file and return it as a Python object.
-    
-    :param filename: The path to the JSON file.
-    :return: A Python object representing the JSON data.
+    param filename: The path to the JSON file.
+    return: A Python object representing the JSON data.
     """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
         return data
-    except FileNotFoundError:
-        print(f"Error: The file {filename} was not found.")
-    except json.JSONDecodeError:
-        print(f"Error: The file {filename} is not a valid JSON file.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
