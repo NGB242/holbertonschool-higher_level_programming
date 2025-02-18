c786171665b0abeb67a4d66824f19f0e6be02cd3
NGB242 <ngbwarren@gmail.com>
2025-02-18 08:41:15 +0100

    more corrections are done

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -2,35 +2,20 @@
 
 import xml.etree.ElementTree as ET
 
-
-def serialize_to_xml(dictionary, filename):
-    """
-    Create the root element
-    """
+def serialize_to_xml(data, filename):
     root = ET.Element("root")
-
-    """
-    Iterate through the dictionary items and add them as child elements
-    """
+    
     for key, value in data.items():
         element = ET.SubElement(root, key)
         element.text = str(value)
 
-    """
-    Create an ElementTree object and write it to the provided filename
-    """
     tree = ET.ElementTree(root)
     with open(filename, "wb") as f:
         tree.write(f)
 
-
 def deserialize_from_xml(filename):
-
-    """
-    Parse the XML file
-    """
     tree = ET.parse(filename)
     root = tree.getroot()
-
+    
     data = {child.tag: child.text for child in root}
     return data
