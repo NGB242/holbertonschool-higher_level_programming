ed00527435f6cd3dedf08db0d4ac22201501305f
NGB242 <ngbwarren@gmail.com>
2025-02-18 07:58:28 +0100

    display the correction of task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Initialize the attributes of the custom object
 """
@@ -8,48 +8,34 @@ import pickle
 
 
 class CustomObject:
-    def __init__(self,    name,    age,    is_student):
+    def __init__(self, name, age, is_student):
 
-    """
-    Initialize the attributes of the custom object.
-    """
-    self.name = name
-    self.age = age
-    self.is_student = is_student
+        """
+        Initialize the attributes of the custom object.
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
     def display(self):
         """
         Display the attributes of the object.
         """
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
+        print(f"Name: {self.name}, Age: {self.age}, Is Student: {self.is_student}")
+
 
     def serialize(self, filename):
         """
         Serialize the object and save it to the specified file.
         """
-        try:
-            with open(filename, 'wb') as file:
-                pickle.dump(self, file)
-        except Exception as e:
-            """
-            Handle any exceptions that occur during serialization.
-            """
-            print(f"Serialization error: {e}")
-            return None
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
 
     @classmethod
     def deserialize(cls, filename):
         """
         Load and return an instance of CustomObject from the specified file.
         """
-        try:
-            with open(filename, 'rb') as file:
-                return pickle.load(file)
-        except Exception as e:
-            """
-            Handle any exceptions that occur during deserialization.
-            """
-            print(f"Deserialization error: {e}")
-            return None
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
