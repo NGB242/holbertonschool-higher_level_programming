93c08d57ba5aff9e7bda97fb7b31ed539defcce6
NGB242 <ngbwarren@gmail.com>
2025-02-28 10:25:56 +0100

    correction of task

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@ def search_replace(my_list, search, replace):
     """
     Create a new list with replaced elements
     """
-    new_list = [replace if element == search else element for element in my_list]
+    new_list = [replace if elmnt == search else elmnt for elmnt in my_list]
     return new_list
