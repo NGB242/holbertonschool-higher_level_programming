f2f741fe0611207817e4c2a113f87b01152dd97e
NGB242 <ngbwarren@gmail.com>
2025-03-02 19:46:13 +0100

    display the result of a function that replaces or adds key/value in a dictionary.

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Update the dictionary with the new key/value pair
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Update the dictionary with the new key/value pair
+    """
+    a_dictionary[key] = value
+    return a_dictionary
