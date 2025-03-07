bd7b1d8293fac665e21ec7e4e74bf3813f0d135b
NGB242 <ngbwarren@gmail.com>
2025-03-07 16:05:27 +0100

    display the result of script

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+/*
+script that prints the following description of the table
+ first_table from the database hbtn_0c_0 in your MySQL server.
+*/
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = DATABASE();
