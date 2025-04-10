59922d6cd57c15e093ce435d90b6176aea8cd6d5
NGB242 <ngbwarren@gmail.com>
2025-04-10 07:53:44 +0200

    display all result of tasks

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,26 +1,56 @@
 #!/usr/bin/python3
+
 """
-Generates Pascal's Triangle up to the nth row.
+    Returns a list of lists of integers representing the Pascals triangle of n:
 """
 
 
 def pascal_triangle(n):
     """
-    Generates Pascal's Triangle up to the nth row.
+    pascal_triangle
 
-    param n: The number of rows in Pascal's Triangle.
-    return: A list of lists representing Pascal's Triangle.
+    Build the next row of Pascal's triangle
+    1. Start with 1
+    2. Compute values as the sum of adjacent elements in current_list
+    3. End with 1
+    4. Update current_list and append new_list to pascal_list
+
+    Args:
+        n (int): iteration on pascal triangle
+
+    Returns:
+        list: list of lists of integers
     """
 
-    if n <= 0:
-        return []
+    # Define variable
+    pascal_list = []
+    current_list = []
+    new_list = []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    
-    return triangle
+    # Check sepcial case
+    if n <= 0:
+        return pascal_list
+
+    # Loop on triangle, (two first case are special case)
+    for index in range(n):
+        if index == 0:
+            pascal_list.append([1])
+
+        elif index == 1:
+            pascal_list.append([1, 1])
+            current_list.extend([1, 1])
+
+        else:
+            new_list = []
+            new_list.append(1)
+
+            for sub_index in range(len(current_list) - 1):
+                value = current_list[sub_index] + current_list[sub_index+1]
+
+                new_list.append(value)
+
+            new_list.append(1)
+            current_list = new_list
+            pascal_list.append(new_list)
+
+    return pascal_list
