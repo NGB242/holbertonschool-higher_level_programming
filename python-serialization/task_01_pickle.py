a5c52ed5dcfc2c7e792e95be68c40a827a188dc6
NGB242 <ngbwarren@gmail.com>
2025-04-10 08:10:55 +0200

    display the result of task

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,17 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Initialize the attributes of the custom object
+    serialize and deserialize custom Python objects using the pickle module
 """
-
 
 import pickle
 
 
 class CustomObject:
-    def __init__(self, name, age, is_student):
+    """
+     Define a custom object
+    """
 
+    def __init__(self, name, age, is_student):
         """
-        Initialize the attributes of the custom object.
+        __init__ constructor
+
+        Args:
+            name (string): name of the user
+            age (int): age of the user
+            is_student (bool): true / false if is a student
         """
         self.name = name
         self.age = age
@@ -19,23 +26,45 @@ class CustomObject:
 
     def display(self):
         """
-        Display the attributes of the object.
+        display the attributes
         """
-        print(f"Name: {self.name}, Age: {self.age}, Is Student: {self.is_student}")
-
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
         """
-        Serialize the object and save it to the specified file.
-        """
-        with open(filename, 'wb') as file:
-            pickle.dump(self, file)
+        serialize with pickle
 
+        Args:
+            filename (string): filename
+
+        Returns:
+            _type_: _descrption_
+        """
+        try:
+            with open(filename, "wb") as myfile:
+                pickle.dump(self, myfile)
+            return True
+        except (FileNotFoundError, IOError, pickle.PicklingError):
+            return None
 
     @classmethod
     def deserialize(cls, filename):
         """
-        Load and return an instance of CustomObject from the specified file.
+        deserialize abstract method to deserialize with
+
+        Args:
+            filename (_type_): _description_
+
+        Returns:
+            _type_: _description_
         """
-        with open(filename, 'rb') as file:
-            return pickle.load(file)
+        try:
+            with open(filename, "rb") as myfile:
+                loaded_data = pickle.load(myfile)
+                if isinstance(loaded_data, cls):
+                    return loaded_data
+                return None
+        except (FileNotFoundError, IOError, pickle.UnpicklingError, EOFError):
+            return None
